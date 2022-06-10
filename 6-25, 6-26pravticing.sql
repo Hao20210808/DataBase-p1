249202bc0f14d2d57340e1d9acd9bcb9334c7590
HaoZhong <88621562+Hao20210808@users.noreply.github.com>
2022-06-10 16:04:06 +0800

    Create 6-25, 6-26pravticing.sql

---
+++ 6-25, 6-26pravticing.sql
@@ -0,0 +1,26 @@
+Q25:
+SELECT Faculty Name 
+    FROM FACULTY,QUALIFIED 
+        WHERE QUALIFIED.FacultyID = FACULTY. FacultyID = ‘ISM 3113’;
+
+Q26:
+a:
+
+
+b:
+SELECT FACULTY.FacultyID, QUALIFIED.FacultyID, 
+    QUALIFIED.CourseID, COURSE.CourseID,
+    FACULTY.FacultyName, COURSE.CourseName
+        FROM FACULTY, QUALIFIED, COURSE
+            WHERE FACULTY.FacultyID = QUALIFIED.FacultyID 
+            AND QUALIFIED.CourseID = COURSE.CourseID
+            AND FACULTY.FacultyName = 'Berndt';
+
+c:
+SELECT StudentID, StudentName, CourseID, SectionNo, SemesterFROM Section  
+    JOIN Registration  
+    ON SectionNo= SectionNoJOIN Student  
+    ON StudentID= StudentIDWHERE SectionNo=2714
+        AND Semester=‘I-2008’
+        AND Semester=‘I-2008’
+            ORDER BY StudentName;
